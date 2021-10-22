c61a8cbd2e2a1c8ae313886a0b279ecbdf3dcb28
Olumba solomon <olumbasolomon08@gmail.com>
2021-10-22 22:23:24 +0100

    creating an environment variable for our commerce.js key, creating an instance from commerce.js library and fetching the data from our commerce,js backend.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import {Products, NavBar} from './components';
-class App extends Component {
-    render(){
-        return (
-            <div>
-                <NavBar />
-                <Products />
-            </div>
-        )
+import {commerce} from './lib/commerce';
+
+const App = () => {
+    const [products, setProducts] = useState([]);
+
+    //fetch data from commerce js using an Async function
+    const fetchProducts = async () => {
+        //destructure the data gotten
+        const {data} = await commerce.products.list();
+        
+        //update the state to that data
+        setProducts(data);
     }
+
+    //load the products on load using useEffect hook
+    useEffect(() => {
+        return fetchProducts()
+    }, []);
+
+    return (
+        <div>
+            <NavBar />
+            <Products />
+        </div>
+    )
 }
 
 export default App;
